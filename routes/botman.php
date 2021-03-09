efd742b361824d00d1eff76998cc305b24c14a9d
Oleh Mordach <omordach@gmail.com>
2021-03-09 12:11:46 +0200

    Added telegram driver

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,21 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('Привіт', function ($bot) {
+    $bot->reply('Вітаю!');
 });
+
+$botman->hears('Добрий ранок', function ($bot) {
+    $bot->reply('Доброго ранку!');
+});
+
+$botman->hears('Добрий день', function ($bot) {
+    $bot->reply('Доброго дня!');
+});
+
+$botman->hears('Добрий вечір', function ($bot) {
+    $bot->reply('Доброго вечора!');
+});
+
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
